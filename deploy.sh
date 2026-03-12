10063c9376d6884671cb9e88114308c2fd0538b5
qifeng <977765310@qq.com>
2026-03-12 21:19:23 +0800

    deploy: 修改静态文件目录为 /var/www/opencodeshare

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ echo "开始构建..."
 BASE_URL=/ npm run build
 
 # 3. 移动到 nginx 目录
-NGINX_DIR="/var/www/html"
+NGINX_DIR="/var/www/opencodeshare"
 BACKUP_DIR="/usr/share/nginx/html_backup_$(date +%Y%m%d_%H%M%S)"
 
 echo "备份当前 nginx 目录..."
